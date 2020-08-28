75d2f39744b6ad58d818bc7dc342de2ebdfed20e
davide-amabile <davideamabile81@gmail.com>
2020-08-28 16:31:19 +0200

    funzione genera numeri senza controllo

--- js/script.js
+++ js/script.js
@@ -1,13 +1,17 @@
-// generare 16 numeri random da 1 a 16
-for ( var i = 1; i <= 5; i++) {
-  var numCpu =  numRdnFrom1(1, 100);
+// // generare 16 numeri random da 1 a 16
+// for ( var i = 1; i <= 5; i++) {
+  var numCpu = numRdnFrom1(1, 100);
   console.log(numCpu);
-}
+// }
 
 
 // funzione generare numeri random da 1
 
 function numRdnFrom1 (min, max) {
-  var numRand = Math.floor(Math.random() * (max - min + 1) ) + min;
-  return numRand;
+  var bomb = [];
+  for ( var i = 1; i <= 5; i++) {
+  var numRandom =Math.floor(Math.random() * (max - min + 1) ) + min;
+  bomba.push(numRandom);
+}
+  return bomba;
 }
