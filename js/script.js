9a6df5a5a958561f471b585a60c905791cccb262
davide-amabile <davideamabile81@gmail.com>
2020-08-28 15:30:04 +0200

    gnerare i 16 numeri

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-alert();
+// generare 16 numeri random da 1 a 16
+for ( var i = 1; i <= 5; i++) {
+  var numCpu =  numRdnFrom1(1, 100);
+  console.log(numCpu);
+}
+
+
+// funzione generare numeri random da 1
+
+function numRdnFrom1 (min, max) {
+  var numRand = Math.floor(Math.random() * (max - min + 1) ) + min;
+  return numRand;
+}
