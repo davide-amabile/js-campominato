41460ac303ebfdee77f4260788fba4808e4a9b32
davide-amabile <davideamabile81@gmail.com>
2020-08-28 17:15:36 +0200

    funzione check

--- js/script.js
+++ js/script.js
@@ -1,17 +1,32 @@
 // // generare 16 numeri random da 1 a 16
 // for ( var i = 1; i <= 5; i++) {
-  var numCpu = numRdnFrom1(1, 100);
+  var numCpu = numRdnFrom1(1, 2);
   console.log(numCpu);
 // }
+number = 2;
+
+var controllo = checkNum(numCpu, number );
+console.log(controllo)
 
 
 // funzione generare numeri random da 1
 
 function numRdnFrom1 (min, max) {
-  var bomb = [];
+  var lista = [];
   for ( var i = 1; i <= 5; i++) {
   var numRandom =Math.floor(Math.random() * (max - min + 1) ) + min;
-  bomba.push(numRandom);
+  lista.push(numRandom);
 }
-  return bomba;
+  return lista;
+}
+
+// funzione controllo numero
+function checkNum (str, num) {
+  var lista = [str];
+  console.log(lista);
+  var numero = num;
+  console.log(num);
+  var check = lista.includes(numero);
+  console.log(check);
+  return check;
 }
