1bee3b44167f1d4f911cff26ff39d889552185bc
davide-amabile <davideamabile81@gmail.com>
2020-08-31 08:57:37 +0200

    numeri random

--- js/script.js
+++ js/script.js
@@ -1,29 +1,25 @@
 // // generare 16 numeri random da 1 a 100
-  var bomba = numRdnFrom1(1, 10);
+  var bomba = numRdnFrom1(1, 10, 5);
   console.log(bomba);
 
 // chiedere all'utente di inserire 16 numeri se il numero è uguale ad un numero random ha perso se no continuare
-// var numUtente = [];
-// console.log(numUtente);
+  // var numUtente = [];
+  //
+  // var i = 0;
+  // while ( i < 5 ) {
+  //   var numIns = parseInt(prompt("Inserire un numero");
+  //
+  //   i++;
+  // }
 
-for ( i = 0; i < 5; i++) {
-  var numUtente = [];
-  var numIns = parseInt(prompt("Inserire un numero"));
-  if (bomba.indexOf(numUtente) === -1) {
-    numUtente.push(numIns);
-  } else {
-    alert("hai perso!!! numero già iserito");
-  }
-  console.log(numUtente);
-}
 
 
 
 // funzione generare numeri random da 1 a 100 non ripetuti
-function numRdnFrom1 (min, max) {
+function numRdnFrom1 (min, max, numRdn) {
   var lista = [];
   var i = 1;
-  while ( i <= 5) {
+  while ( i <= numRdn) {
   var numRandom =Math.floor(Math.random() * (max - min + 1) ) + min;
   if ( lista.indexOf(numRandom) === -1 ) {
     lista.push(numRandom);
